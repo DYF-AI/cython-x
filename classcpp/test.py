845becdae5782e4669d4cadc1a4843d4beb3a4a1
DYF-AI <https://github.com/DYF-AI/>
2021-06-08 23:23:56 +0800

    sort

--- classcpp/test.py
+++
@@ -1,8 +0,0 @@
-import rect
-print("Cython")
-x0, y0, x1, y1 = 1, 2, 3, 4
-rect_obj = rect.PyRectangle(x0, y0, x1, y1)
-print("Size: ", rect_obj.get_size())
-print("Area: ", rect_obj.get_area())
-
-print("x0: ", rect_obj.x0)
--- compareNumpy/compute_np.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def compute_np(array_1, array_2, a, b, c):
-    return np.clip(array_1, 2, 10) * a + array_2 * b + c
-    
